ef377c25b8b2821228df0b7ee05b6ffa8770879a
cmakihara <clmakihara@gmail.com>
2019-03-09 21:01:18 -0300

    Cidade, Cliente, Endereco, Estado e TipoCliente.

--- src/main/java/com/maki/api/ApiApplication.java
+++ src/main/java/com/maki/api/ApiApplication.java
@@ -9,8 +9,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.maki.api.model.Categoria;
+import com.maki.api.model.Cidade;
+import com.maki.api.model.Cliente;
+import com.maki.api.model.Endereco;
+import com.maki.api.model.Estado;
 import com.maki.api.model.Produto;
+import com.maki.api.model.enums.TipoCliente;
 import com.maki.api.repository.CategoriaRepository;
+import com.maki.api.repository.CidadeRepository;
+import com.maki.api.repository.ClienteRepository;
+import com.maki.api.repository.EnderecoRepository;
+import com.maki.api.repository.EstadoRepository;
 import com.maki.api.repository.ProdutoRepository;
 
 @SpringBootApplication
@@ -18,8 +27,21 @@ public class ApiApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		
@@ -43,8 +65,36 @@ public class ApiApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerias");
+		Estado est2 = new Estado(null, "Sao Apulo");
+		
+		Cidade c1 = new Cidade(null, "uberlanmdia", est1);
+		Cidade c2 = new Cidade(null, "São paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		
+
 	}
 }
