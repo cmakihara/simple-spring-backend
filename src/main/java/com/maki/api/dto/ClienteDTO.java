a83c93f713ee825ded916357d8a2b40e1f5b0a18
cmakihara <clmakihara@gmail.com>
2019-04-02 12:38:03 -0300

    Validação de email no update.

--- src/main/java/com/maki/api/dto/ClienteDTO.java
+++ src/main/java/com/maki/api/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.maki.api.model.Cliente;
+import com.maki.api.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/maki/api/service/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.maki.api.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.maki.api.dto.ClienteDTO;
+import com.maki.api.model.Cliente;
+import com.maki.api.repository.ClienteRepository;
+import com.maki.api.resource.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>)request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer urlId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();		
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if( aux != null && !aux.getId().equals(urlId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
