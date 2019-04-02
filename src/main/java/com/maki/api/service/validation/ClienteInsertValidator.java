651a428ab796dd81c3e63b2a2194d809fe5244b1
cmakihara <clmakihara@gmail.com>
2019-04-02 11:49:52 -0300

    Validação de email repetido.

--- src/main/java/com/maki/api/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/maki/api/service/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.maki.api.dto.ClienteNewDTO;
+import com.maki.api.model.Cliente;
 import com.maki.api.model.enums.TipoCliente;
+import com.maki.api.repository.ClienteRepository;
 import com.maki.api.resource.exception.FieldMessage;
 import com.maki.api.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if( aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
