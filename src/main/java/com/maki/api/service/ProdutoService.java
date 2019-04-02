c75a9dac9e5ba84a69877c5acff50f113120e427
cmakihara <clmakihara@gmail.com>
2019-04-02 15:42:45 -0300

    Busca de produtos por nome.

---
+++ src/main/java/com/maki/api/service/ProdutoService.java
@@ -0,0 +1,58 @@
+package com.maki.api.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.maki.api.model.Categoria;
+import com.maki.api.model.Produto;
+import com.maki.api.repository.CategoriaRepository;
+import com.maki.api.repository.ProdutoRepository;
+import com.maki.api.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> produto = produtoRepository.findById(id);
+		return produto.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public List<Produto> findAll() {
+		return produtoRepository.findAll();
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return produtoRepository.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	}
+	
+	public Produto insert(Produto produto) {
+		produto.setId(null);
+		return produtoRepository.save(produto);
+	}
+	
+	public Produto update(Produto produto) {
+		
+		return produtoRepository.save(produto);
+	}
+	
+	public void delete(Integer id) {
+		
+		produtoRepository.deleteById(id);
+	}
+
+}
