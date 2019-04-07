cd75092428d2b45fd8abed2cc6eec16129fd406a
cmakihara <clmakihara@gmail.com>
2019-04-06 23:35:04 -0300

    Cliente recupera somente ele.

--- src/main/java/com/maki/api/service/ClienteService.java
+++ src/main/java/com/maki/api/service/ClienteService.java
@@ -17,35 +17,44 @@ import com.maki.api.dto.ClienteNewDTO;
 import com.maki.api.model.Cidade;
 import com.maki.api.model.Cliente;
 import com.maki.api.model.Endereco;
+import com.maki.api.model.enums.Perfil;
 import com.maki.api.model.enums.TipoCliente;
 import com.maki.api.repository.ClienteRepository;
 import com.maki.api.repository.EnderecoRepository;
+import com.maki.api.security.UserSS;
+import com.maki.api.service.exceptions.AuthorizationException;
 import com.maki.api.service.exceptions.DataIntegrityException;
 import com.maki.api.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	
+
 	public Cliente find(Integer id) {
+
+		UserSS user = UserService.autheticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
 		Optional<Cliente> cliente = clienteRepository.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-	
+
 	}
 
 	public List<Cliente> findAll() {
 		return clienteRepository.findAll();
 	}
-	
+
 	@Transactional
 	public Cliente insert(Cliente cliente) {
 		cliente.setId(null);
@@ -53,7 +62,7 @@ public class ClienteService {
 		enderecoRepository.saveAll(cliente.getEnderecos());
 		return cliente;
 	}
-	
+
 	public Cliente update(Cliente cliente) {
 		Cliente newObj = find(cliente.getId());
 		updateData(newObj, cliente);
@@ -69,35 +78,36 @@ public class ClienteService {
 		find(id);
 		try {
 			clienteRepository.deleteById(id);
-			
+
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir possui entidade relacionadas");
 		}
 	}
-	
+
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return clienteRepository.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromDTO(ClienteDTO clienteDTO) {
 		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null, null);
 	}
-	
+
 	public Cliente fromDTO(ClienteNewDTO objDto) {
-		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfCnpj(), TipoCliente.toEnum(objDto.getTipoCliente()), bCryptPasswordEncoder.encode(objDto.getSenha()));
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfCnpj(),
+				TipoCliente.toEnum(objDto.getTipoCliente()), bCryptPasswordEncoder.encode(objDto.getSenha()));
 		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
-		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
 		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDto.getTelefone1());
-		if (objDto.getTelefone2()!=null) {
+		if (objDto.getTelefone2() != null) {
 			cli.getTelefones().add(objDto.getTelefone2());
 		}
-		if (objDto.getTelefone3()!=null) {
+		if (objDto.getTelefone3() != null) {
 			cli.getTelefones().add(objDto.getTelefone3());
 		}
 		return cli;
 	}
-	
-	
+
 }
---
+++ src/main/java/com/maki/api/service/UserService.java
@@ -0,0 +1,17 @@
+package com.maki.api.service;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.maki.api.security.UserSS;
+
+public class UserService {
+
+	public static UserSS autheticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
